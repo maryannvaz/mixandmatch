8dccb9adb4fec5e15801151ae6855043c6b89695
maryannvazhapilly <73087433+maryannvazhapilly@users.noreply.github.com>
2021-01-12 15:52:31 -0500

    Link pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import AboutUs from './AboutUs';
 import Contact from './Contact';
 import cover from '../assets/cover.png';
@@ -13,7 +14,7 @@ class App extends Component {
                 <img src={cover} alt='cover' className='actualcover' />
                 <h1>Welcome to Mix and Match!</h1>
                 <hr />
-                <a href='/tops'><img src={tops} alt='tops' className='cover' /></a>
+                <Link to='/tops'><img src={tops} alt='tops' className='cover' /></Link>
                 <a href='/outfits'><img src={outfit} alt='outfit' className='cover' /></a>
                 <a href='/bottoms'><img src={bottoms} alt='bottoms' className='cover' /></a>
                 <hr />
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Header from './components/Header';
+import Tops from './components/Tops';
+import Outfits from './components/Outfits';
+import Bottoms from './components/Bottoms';
 import './index.css';
 
 ReactDOM.render(
     <Router history={createBrowserHistory()}>
         <Switch>
             <Route exact path='/' render={() => <Header><App /></Header>} />
-            <Route exact path='/' render={() => <Header><Tops /></Header>} />
-            <Route exact path='/' render={() => <Header><Bottoms /></Header>} />
+            <Route exact path='/tops' render={() => <Header><Tops /></Header>} />
+            <Route exact path='/outfits' render={() => <Header><Outfits /></Header>} />
+            <Route exact path='/bottoms' render={() => <Header><Bottoms /></Header>} />
         </Switch>
     </Router>, 
     document.getElementById('root')
