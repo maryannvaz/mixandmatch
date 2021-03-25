0f1befa109530a8d02e2ecc64b1699ed2e3026a9
Miranda Song <66978315+mirandasong24@users.noreply.github.com>
2021-03-25 13:46:43 -0400

    moved out index.html,css,js & changed .json /src

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -35909,7 +35909,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54889" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52985" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
