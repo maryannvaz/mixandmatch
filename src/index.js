a2a71602bfc60220fb1c0d3b527a6365d569a8a0
Miranda Song <66978315+mirandasong24@users.noreply.github.com>
2021-03-25 14:58:15 -0400

    put index.js in src

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-import App from './components/App';
-import Header from './components/Header';
-import Tops from './components/Tops';
-import Outfits from './components/Outfits';
-import Bottoms from './components/Bottoms';
-import './index.css';
+import App from '../components/App';
+import Header from '../components/Header';
+import Tops from '../components/Tops';
+import Outfits from '../components/Outfits';
+import Bottoms from '../components/Bottoms';
+import '../index.css';
 
 ReactDOM.render(
     <Router history={createBrowserHistory()}>
