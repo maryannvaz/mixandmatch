4ba42ae4facc5d6857eecbe2542d938b0f12997c
Miranda Song <66978315+mirandasong24@users.noreply.github.com>
2021-03-27 15:11:50 -0400

    changed filter brightness(90%)

--- dist/mixandmatch.e31bb0bc.js
+++ dist/mixandmatch.e31bb0bc.js
@@ -35733,7 +35733,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52925" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65031" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
